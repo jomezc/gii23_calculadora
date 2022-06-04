738c00816fcefb43ee7797d6301352b720d1768b
jomezc <73670380+jomezc@users.noreply.github.com>
2022-06-04 17:25:29 +0200

    Definición del método testResta para prueba unitaria del metodo resta  de calculadora

--- test/Test_calc.py
+++ test/Test_calc.py
@@ -13,10 +13,11 @@ class TestCalc(unittest.TestCase):
     def setUp(self):
         self.calculadora = Calculadora()
         self.datos_suma = [[2, 2, 4], [-2, -2, -4], [2, -2, 0], [350.25, 100.75, 451], [200, 100.5, 300.5]]
+        self.datos_resta = [[2, 2, 0], [-2, -2, 0], [2, -2, 4], [350, 100.75, 249.25], [200, 100.5, 300.5]]
 
     def testSuma(self):
         """
-        Función de test de suma de elementos con un esperado
+        Método de test de suma de elementos con un esperado
         :param datos: Lista con las listas de datos de prueba
         """
         # arrrange, Inicializamos los parámetros de suma y para cada caso de la lista de casos
@@ -30,6 +31,21 @@ class TestCalc(unittest.TestCase):
             error = f'la suma de {a} + {b} debería ser {esperado}'
             self.assertEqual(resultado, esperado, error)
 
+    def testResta(self):
+        """
+        Método de test de suma de elementos con un esperado
+        :param datos: Lista con las listas de datos de prueba
+        """
+        # arrrange, Inicializamos los parámetros de suma y para cada caso de la lista de casos
+        for dato in self.datos_resta:
+            a = dato[0]
+            b = dato[1]
+            esperado = dato[2]
+            # act,  invocamos al método suma
+            resultado = self.calculadora.resta(a, b)
+            # asser, validamos los resultados
+            error = f'la suma de {a} - {b} debería ser {esperado}'
+            self.assertEqual(resultado, esperado, error)
 #   MAIN
 if __name__ == '__main__':
     unittest.main()
